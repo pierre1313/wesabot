3fcc6ae6ee5a4b6ff2db164439ac45e9795a897f
Andre Arko <andre@arko.net>
2011-02-21 04:43:14 -0800

    clean up kibitz, and just for elan

--- campfire/polling_bot/plugins/kibitz_plugin.rb
+++ campfire/polling_bot/plugins/kibitz_plugin.rb
@@ -24,27 +24,34 @@ class KibitzPlugin < Campfire::PollingBot::Plugin
     when /(wake up|you awake)/i
       bot.say("Yo.")
     when /zod/i
-      sayings = ["Zod's a wanker.",
-                 "I'd tell you about Zod, but you wouldn't listen. No one ever does.",
-                 "somebody send Zod back to the Phantom Zone",
-                 "Zod and I were friends, once. It all ended one awful night in El Paso over a bottle of shitty scotch and a ten-dollar whore. Christ, those were the days."]
-      bot.say_random(sayings)
+      bot.say_random [
+        "Zod's a wanker.",
+        "I'd tell you about Zod, but you wouldn't listen. No one ever does.",
+        "somebody send Zod back to the Phantom Zone",
+        "Zod and I were friends, once. It all ended one awful night in El Paso over a bottle of shitty scotch and a ten-dollar whore. Christ, those were the days."
+      ]
     when /thanks|thank you/i
-      sayings = ["No problem.", "np", "any time", "that's what I'm here for", "You're welcome."]
-      bot.say_random(sayings)
+      bot.say_random ["No problem.", "np", "any time", "that's what I'm here for", "You're welcome."]
+    when /jessica alba/i
+      bot.say_random [
+        "Jessica Alba is my dream girl",
+        "no question, the hottest girl ever",
+        "yeah... er, what was I saying?",
+        "she really is incredibly hot, you know",
+      ]
     else
-      sayings = ["I have no idea what you're talking about, #{person}.",
-                 "eh?",
-                 "oh, interesting",
-                 "say more, #{person}",
-                 "#{person}, you do realize that you're talking to a bot with a very limited vocabulary, don't you?",
-                 "Whatever, #{person}.",
-                 "Marc, tell #{person} to leave me alone.",
-                 "Not now, #{person}.",
-                 "brb crying",
-                 "How do you feel when someone says '#{message.command}' to you, #{person}?"]
-
-      bot.say_random(sayings)
+      bot.say_random [
+        "I have no idea what you're talking about, #{person}.",
+        "eh?",
+        "oh, interesting",
+        "say more, #{person}",
+        "#{person}, you do realize that you're talking to a bot with a very limited vocabulary, don't you?",
+        "Whatever, #{person}.",
+        "Marc, tell #{person} to leave me alone.",
+        "Not now, #{person}.",
+        "brb crying",
+        "How do you feel when someone says '#{message.command}' to you, #{person}?"
+      ]
     end
   end
 end
