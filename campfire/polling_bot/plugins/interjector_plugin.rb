b58e51df5cf180ec422cfb2e456b14598856f66f
Andre Arko <andre@arko.net>
2011-02-24 01:06:54 -0800

    create InterjectorPlugin for non-commands (like double facepalm)

---
+++ campfire/polling_bot/plugins/interjector_plugin.rb
@@ -0,0 +1,39 @@
+# Plugin to make Wes actively comment (and maybe super annoying)
+class InterjectorPlugin < Campfire::PollingBot::Plugin
+  accepts :text_message
+  priority -1
+
+  def process(message)
+    person = message.person
+    case message.command || message.body
+    when /jessica alba/i
+      bot.say_random [
+        "Jessica Alba is my dream girl",
+        "no question, the hottest girl ever",
+        "yeah... er, what was I saying?",
+        "she really is incredibly hot, you know",
+      ]
+    when /^(facepalm|EFACEPALM|:fp|:facepalm:|\*facepalm\*|m\()$/i
+      bot.say_random [
+        # picard facepalm
+        "https://img.skitch.com/20110224-875h7w1654tgdhgrxm9bikhkwq.jpg",
+        # polar bear facepalm
+        "https://img.skitch.com/20110224-bsd2ds251eit8d3t1y2mkjtfx8.jpg"
+      ]
+    when /^(double facepalm|EDOUBLEFACEPALM|:fpfp|:doublefacepalm:|m\( m\()$/i
+      # picard + riker facepalm
+      bot.say "https://img.skitch.com/20110224-ncacgpudhfr2s4te6nswenxaqt.jpg"
+    when /^i see your problem/i
+      # pony mechanic
+      bot.say "https://img.skitch.com/20110224-8fmfwdmg6kkrcpijhamhqu7tm6.jpg"
+    when /^(wfm|works for me)$/
+      # works on my machine
+      bot.say "https://img.skitch.com/20110224-jrcf6e4gc936a2mxc3mueah2in.png"
+    when /^(stacktrace or gtfo|stacktrace or it didn't happen|stacktrace!)$/
+      # stacktrace or gtfo
+      bot.say "https://img.skitch.com/20110224-pqtmiici9wp9nygqi4nw8gs6hg.png"
+    end
+  end
+
+
+end
